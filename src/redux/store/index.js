484bf186ebbeea4fdc31362ddc0e2073f38547e6
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-01 02:20:27 +0700

    Use sessionStorage to save and load store's state

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,21 @@ import { combineReducers, createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
-import auth from './reducers/auth'
+import { loadState, saveState } from './sessionStorage'
+import auth from '../reducers/auth'
 
 const rootReducer = combineReducers({ auth })
+const persistedState = loadState()
 
 const store = createStore(
   rootReducer,
+  persistedState,
   composeWithDevTools(applyMiddleware(thunk))
 )
+
+saveState(store.getState())
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 export default store
