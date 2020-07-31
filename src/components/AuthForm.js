a1087864fca8a2b526cec2af71f85b5e24fb4fe7
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-01 00:45:19 +0700

    Try to handle login in AuthForm

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import connect from 'react-redux'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import { login } from '../redux/actions/auth'
 
-const AuthForm = () => {
+const AuthForm = ({ isAuthenticated, isLoading, error, handleLogin }) => {
+  console.info({ isAuthenticated, isLoading, error })
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        handleLogin()
+      }}
+    >
       <h1>Enter pass key:</h1>
       <input type='text' />
       <input type='submit' value='Login' />
@@ -25,6 +33,13 @@ const mapDispatchToProps = (dispatch) => {
   return {
     handleLogin: (apiKey) => dispatch(login(apiKey))
   }
+}
+
+AuthForm.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  error: PropTypes.any,
+  handleLogin: PropTypes.func
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AuthForm)
